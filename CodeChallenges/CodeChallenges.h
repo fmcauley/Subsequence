6608d4073f0a7e7239d901edbcce064336af7e34
Frank McAuley <frank.mcauley@gmail.com>
2020-10-16 10:38:23 -0400

    Initial Commit

---
+++ CodeChallenges/CodeChallenges.h
@@ -0,0 +1,18 @@
+//
+//  CodeChallenges.h
+//  CodeChallenges
+//
+//  Created by Frank McAuley on 10/16/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CodeChallenges.
+FOUNDATION_EXPORT double CodeChallengesVersionNumber;
+
+//! Project version string for CodeChallenges.
+FOUNDATION_EXPORT const unsigned char CodeChallengesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CodeChallenges/PublicHeader.h>
+
+
